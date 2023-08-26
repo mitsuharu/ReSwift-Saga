883f40cb6537995c652a7eba2f9da373c6ef1795
Mitsuharu Emoto <m_emoto@yumemi.co.jp>
2023-08-26 16:58:28 +0900

    add Example Project

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by 江本 光晴 on 2023/08/26.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
