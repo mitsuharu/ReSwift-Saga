f504ea87e7ce3eb00566180d2c6e151087018587
Mitsuharu Emoto <m_emoto@yumemi.co.jp>
2023-10-07 11:13:41 +0900

    fix

--- Sources/ReSwiftSaga/InternalBridge.swift
+++ Sources/ReSwiftSaga/InternalBridge.swift
@@ -18,15 +18,17 @@ final class InternalBridge {
     
     static let shared = InternalBridge()
     private let subject = PassthroughSubject<Action, Error>()
+    private var subscriptions = [AnyCancellable]()
     
     // createSagaMiddleware で Redux 内部の dispatch を取得する
-    // TODO: Optional を外したい
     var dispatch: DispatchFunction? = nil
     
     // createSagaMiddleware で Redux 内部の getState を取得する
-    // TODO: Optional を外したい
-    // TODO: Any ではなく State を指定したいが、どのタイミングで行う？
     var getState: (() -> Any?)? = nil
+    
+    deinit{
+        subscriptions.forEach{ $0.cancel() }
+    }
     
     /**
      action を発行する
@@ -34,13 +36,38 @@ final class InternalBridge {
     func put(_ action: Action){
         subject.send(action)
     }
+        
+    /**
+     特定の action を監視する
+     
+     @decsription
+     Combine の Future の value は iOS 15 以上から利用できる。
+     */
+    @available(iOS 15, macOS 12, *)
+    func take(_ actionType: Action.Type) -> Future <Action, Never> {
+        return Future { [weak self] promise in
+            guard let self = self else {
+                return
+            }
+            self.subject.filter {
+                type(of: $0) == actionType
+            }.sink { [weak self] in
+                self?.complete($0)
+            } receiveValue: {
+                promise(.success($0))
+            }.store(in: &self.subscriptions)
+        }
+    }
     
     /**
      特定の action を監視する
+     
+     @decsription
+     iOS 13, 14 向けの実装です。
      */
     func take(_ actionType: Action.Type, receive: @escaping (_ action: Action) -> Void ){
         // 監視は一度限りで行い、検出後は破棄する
-        // 破棄しないと、検出した action を対応にした場合、withCheckedContinuation で二重呼び出しにカウントされクラッシュする
+        // 破棄しないと withCheckedContinuation で多重呼出の扱いになりクラッシュする
         var cancellable: AnyCancellable? = nil
         cancellable = subject.filter {
             type(of: $0) == actionType
